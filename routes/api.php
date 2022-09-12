5165764dcb1f2bd9fffd306a405987df0860db3d
Aya Mohammed <aya592648@example.com>
2022-09-12 17:24:27 +0200

    ADD files

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('/movies', MovieController::class)->middleware('auth:sanctum');
+
+Route::post('/tokens/create', function (Request $request) {
+
+    $request->validate([
+        'email'    => 'required',
+        'password' => 'required'
+    ]);
+
+    $user = User::whereEmail($request->email)->first();
+
+    if($user){
+
+        $token = $user->createToken('movies');
+ 
+        return ['token' => $token->plainTextToken];
+    }
+    
+});
